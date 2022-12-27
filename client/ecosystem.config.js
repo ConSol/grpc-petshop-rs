29ace82466f21bd43663df52a3e88f1d2c146634
Philipp Renoth <renoth@aitch.de>
2022-12-27 13:19:32 +0100

    client ecosystem.config.js make "npm run dev" autorestart=false, shutdown_with_message=true

--- client/ecosystem.config.js
+++ client/ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
   apps: [
     {
       script: 'npm run dev',
+      autorestart: false,
+      shutdown_with_message: true,
     },
     {
       script: './scripts/protogen.sh',
